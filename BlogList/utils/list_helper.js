b6ac3bec89b4cb68476fc86596114045a04dd81c
Stephen Dorssers <sdorssers7@gmail.com>
2025-02-03 15:18:03 -0500

    exercise 4.5 - testing for most likes in blog list

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -12,5 +12,24 @@ const totalLikes = (blogs) => {
       return sum
     }
 }
+
+const favBlog = (blogs) => {
+    likes = []
+    if (blogs.length === 0) return 0
+
+    for (i=0; i < blogs.length; i++) {
+        likes.push(blogs[i].likes)
+    }
+
+    const maxLikes = Math.max(...likes)
+    const index = likes.findIndex(max => max === maxLikes)
+    const mostLikedBlog = {
+        author: blogs[index].author,
+        likes: blogs[index].likes,
+        title: blogs[index].title
+    }
+
+    return mostLikedBlog
+}
   
-module.exports = { dummy, totalLikes }
+module.exports = { dummy, totalLikes, favBlog }
