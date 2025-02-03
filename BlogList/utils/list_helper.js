cf06d3465e18f9dbefde3c352d37168e113e5a6b
Stephen Dorssers <sdorssers7@gmail.com>
2025-02-03 14:40:19 -0500

    exercise 4.4 - total likes test

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -1,5 +1,16 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    sum = 0
+    if (blogs.length === 0) return 0
+    else {
+      for (i=0; i < blogs.length; i++){
+        sum += blogs[i].likes
+      }
+      return sum
+    }
+}
   
-module.exports = { dummy }
+module.exports = { dummy, totalLikes }
