be7b51fa9d263ce62eb1b99b2041a915a115e2ac
Stephen Dorssers <sdorssers7@gmail.com>
2025-02-13 20:23:41 -0500

    minor user model change

--- BlogList/models/user.js
+++ BlogList/models/user.js
@@ -12,7 +12,10 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true, 
   },
-  name: String,
+  name: {
+    type: String,
+    required: true,
+  },
   passHash: {
     type: String,
     required: true,
